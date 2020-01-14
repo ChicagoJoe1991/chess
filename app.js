2b0dd58a0207348394f81776c27660d062f56089
Walter <eschenbach.walter@gmail.com>
2020-01-14 02:58:04 -0500

    initial board setup

---
+++ app.js
@@ -0,0 +1,10 @@
+let log = console.log;
+log('hello');
+
+$(document).ready(function(){
+    for(i=0;i<8;i++){
+        for(j=0;i<8;i++){
+            $('#container').append('<div id="board">eeee</div>');
+        }
+    };
+});
