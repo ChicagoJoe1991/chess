d44ab8f688b3380c3ee5d0778e2dd62c7a6c60d9
Walter <eschenbach.walter@gmail.com>
2020-01-14 04:33:04 -0500

    board update

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 let log = console.log;
-log('hello');
+let l = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
+//html for the board using css grid
 $(document).ready(function(){
-    for(i=0;i<8;i++){
-        for(j=0;i<8;i++){
-            $('#container').append('<div id="board">eeee</div>');
-        }
+    for(i=8;i>0;i--){
+        for(j=0;j<8;j++){
+            $('#container').append('<div id="space">'+ l[j] + i +'</div>');
+        };
     };
+});
+
+$('#space').append('<img src="knight.svg"/>');
+
+
+$('img').addEventListener('click', (e) => {
+
+
 });
